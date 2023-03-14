7198158b61ed13ec495039c0f1ac87c0b1768cd0
Cleo Gao <celeo127@outlook.com>
2023-03-14 17:26:01 +0800

    finish header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon
+ } from '@heroicons/react/solid';
 export const Header = () => {
     return(
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 lg:">
@@ -16,7 +17,17 @@ export const Header = () => {
                 <input type='text' placeholder='Start your search' className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' />
                 <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
             </div>
-            <div></div>
+
+
+            <div className='flex items-center space-x-4 justify-end text-gray-500'>
+                <p className='hidden md:inline cursor-pointer'>Become a host</p>
+                <GlobeAltIcon className='h-6 cursor-pointer' />
+
+                <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+                    <MenuIcon className='h-6'/>
+                    <UserCircleIcon className='h-6'/>
+                </div>
+            </div>
         </header>
     )
 }
