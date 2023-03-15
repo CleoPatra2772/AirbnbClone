887efdd98b08cca8fac9ad305793dd52bfdf9c78
Cleo Gao <celeo127@outlook.com>
2023-03-15 20:11:40 +0800

    finish first section

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['1000logos.net'], 
-    
+    domains: [
+      '1000logos.net',
+    'links.papareact.com',
+  ], 
+   
   },
   
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Banner } from '../components/Banner'
 import { Header } from '../components/Header'
+import { SmallCard } from '../components/SmallCard'
 
-const Home = () => {
+const Home = ({exploreData}) => {
+  
   return (
     <div className="">
       <Head>
@@ -17,12 +19,39 @@ const Home = () => {
       {/* Banner */}
       <Banner/>
 
-      <main>
-        
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {exploreData?.map(({img, distance, location}) => (
+            <SmallCard 
+            key={img}
+            img = {img} 
+            distance = {distance} 
+            location = {location}/>
+          ))}
+          </div>
+        </section>
+
+        <section>
+          
+        </section>
       </main>
       
     </div>
   )
 }
 
-export default Home
+export default Home;
+
+
+export async function getStaticProps(){
+  const exploreData = await fetch('https://api.npoint.io/f659feeadfe10d9d6c59').then(res => res.json());
+
+  return {
+    props: {
+      exploreData
+    }
+  }
+}
